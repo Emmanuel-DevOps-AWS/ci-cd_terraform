db6330745a5664700ea598f9469294c2d2b8218d
Emma A <emmanuel.abolar@gmail.com>
2024-01-10 18:53:45 -0500

    test again

--- var.tf
+++
@@ -1,8 +0,0 @@
-variable "access_key" {
-    type = string
-    default = "AKIA34RZTGTKFTD5TBXT"
-}
-variable "secret_key" {
-  type = string
-  default = "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"
-}
